d2cb4f59024caaa534f67989ee829940f6a1c509
ashleyannlaz <paperpastels@gmail.com>
2021-09-07 19:08:00 -0400

    Write Model Functions

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,14 +1,14 @@
 exports.up = async function (knex) {
-  await knex.schema.createTable('cars', table => {
-    table.increments()
-    table.text('vin').unique().notNullable()
-    table.text('make').notNullable()
-    table.text('model').notNullable()
-    table.decimal('mileage').notNullable()
-    table.text('title')
-    table.text('transmission')
-});
-}
+  await knex.schema.createTable("cars", (table) => {
+    table.increments();
+    table.text("vin").unique().notNullable();
+    table.text("make").notNullable();
+    table.text("model").notNullable();
+    table.decimal("mileage").notNullable();
+    table.text("title");
+    table.text("transmission");
+  });
+};
 exports.down = async function (knex) {
-  await knex.schema.dropTableIfExists('cars')
+  await knex.schema.dropTableIfExists("cars");
 };
