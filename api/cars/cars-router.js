13e7fd45e0b0e127cca0ce2454cfe4e01425b010
ashleyannlaz <paperpastels@gmail.com>
2021-09-07 19:22:26 -0400

    Write Get API

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+const express = require('express');
+const Cars = require('./cars-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Cars.getAll()
+        res.status(200).json(cars)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
-const express = require("express")
+const express = require("express");
+const carsRouter = require("./cars/cars-router.js");
+const server = express();
 
-const server = express()
+server.use(express.json());
 
-// DO YOUR MAGIC
+server.use("/api/cars", carsRouter);
 
-module.exports = server
+server.use((err, req, res, next) => {
+  //eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
