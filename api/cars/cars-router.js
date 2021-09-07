0aa16db3a63165420043d322aa2dd0323ec8ba23
ashleyannlaz <paperpastels@gmail.com>
2021-09-07 19:26:41 -0400

    Wrote all endpoints

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -12,6 +12,24 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+router.get('/:id', async (req, res, next) => {
+    try {
+        const car = await Cars.getById(req.params.id)
+        res.status(200).json(car)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const possibleCar = await Cars.create(req.body)
+        res.status(201).json(possibleCar)
+    } catch (error) {
+        next(error)
+    }
+})
+
 
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -6,8 +6,7 @@ server.use(express.json());
 
 server.use("/api/cars", carsRouter);
 
-server.use((err, req, res, next) => {
-  //eslint-disable-line
+server.use((err, req, res, next) => {//eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
