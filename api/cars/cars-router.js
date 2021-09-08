e52a351865de7bef3a74219809eac148fcf5ff7f
ashleyannlaz <paperpastels@gmail.com>
2021-09-07 21:06:20 -0400

    MVP

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,35 +1,44 @@
-const express = require('express');
-const Cars = require('./cars-model');
+const express = require("express");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+} = require("./cars-middleware");
+const Cars = require("./cars-model");
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await Cars.getAll();
+    res.status(200).json(cars);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", checkCarId, async (req, res, next) => {
+  try {
+    res.json(req.car);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  async (req, res, next) => {
     try {
-        const cars = await Cars.getAll()
-        res.status(200).json(cars)
+      const possibleCar = await Cars.create(req.body);
+      res.status(201).json(possibleCar);
     } catch (error) {
-        next(error)
+      next(error);
     }
-})
-
-router.get('/:id', async (req, res, next) => {
-    try {
-        const car = await Cars.getById(req.params.id)
-        res.status(200).json(car)
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.post('/', async (req, res, next) => {
-    try {
-        const possibleCar = await Cars.create(req.body)
-        res.status(201).json(possibleCar)
-    } catch (error) {
-        next(error)
-    }
-})
-
-
+  }
+);
 
 module.exports = router;
